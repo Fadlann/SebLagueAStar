371cd279a331f9a0f648149281c5cde257f071a0
Fadlann <55979230+Fadlann@users.noreply.github.com>
2020-04-13 09:28:24 +0700

    Create Node.cs

---
+++ Ep1/Node.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Node{
+    public bool walkable;
+    public Vector3 worldPoint;
+
+    public Node(bool walkable, Vector3 worldPoint)
+    {
+        this.walkable = walkable;
+        this.worldPoint = worldPoint;
+    } 
+}
